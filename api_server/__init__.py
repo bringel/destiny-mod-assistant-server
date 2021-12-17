286a00ddcd08afdb1727bf1bc9e7976a779c932b
Brad Ringel <brad.ringel@gmail.com>
2021-12-17 08:34:13 -0500

    very quick attempt at getting user information after getting the oauth token

--- api_server/__init__.py
+++ api_server/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask, session, redirect, request
-from requests_oauthlib import OAuth2Session
 import os
+
+from flask import Flask, redirect, request, session
+from flask.json import jsonify
+from requests_oauthlib import OAuth2Session
+from sqlalchemy import create_engine, text
 
 
 def create_app():
@@ -12,10 +15,11 @@ def create_app():
     def login():
         bungie = OAuth2Session(os.environ.get("OAUTH_CLIENT_ID"))
         authorization_url, state = bungie.authorization_url(
-            "https://www.bungie.net/en/oauth/authorize"
+            os.environ.get("BUNGIE_AUTHORIZATION_URL")
         )
 
         session["oauth_state"] = state
+        print(state)
         return redirect(authorization_url)
 
     @app.route("/callback")
@@ -24,13 +28,35 @@ def create_app():
             os.environ.get("OAUTH_CLIENT_ID"), state=session["oauth_state"]
         )
         token = bungie.fetch_token(
-            "https://www.bungie.net/platform/app/oauth/token/",
+            os.environ.get("BUNGIE_TOKEN_URL"),
             client_secret=os.environ.get("OAUTH_CLIENT_SECRET"),
             authorization_response=request.url,
         )
 
         session["oauth_token"] = token
 
-        return redirect("/")
+        user = bungie.get(
+            f"https://bungie.net/Platform/Destiny2/254/Profile/{session['oauth_token']['membership_id']}/LinkedProfiles/",
+            headers={"X-API-KEY": os.environ.get("BUNGIE_API_KEY")},
+        ).json()
+
+        print(user)
+        profile = user["Response"]["profiles"][0]
+        membership_type = profile["membershipType"]
+        membership_id = profile["membershipId"]
+        display_name = f"{profile['bungieGlobalDisplayName']}#{profile['bungieGlobalDisplayNameCode']}"
+
+        engine = create_engine(os.environ.get("DB_CONNECTION_STRING"))
+        with engine.begin() as connection:
+            connection.execute(
+                text(
+                    "INSERT INTO users (destiny_membership_type, destiny_membership_id, display_name) VALUES (:membership_type, :membership_id, :display_name) ON CONFLICT DO NOTHING;"
+                ),
+                membership_type=membership_type,
+                membership_id=membership_id,
+                display_name=display_name,
+            )
+
+        return jsonify(user)
 
     return app

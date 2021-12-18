e464f1a668fb008a453e601015e3304de6fea8f4
Brad Ringel <brad.ringel@gmail.com>
2021-12-18 16:22:51 -0500

    add timestamps to users table

--- api_server/tables.py
+++ api_server/tables.py
@@ -1,4 +1,6 @@
-from sqlalchemy import BigInteger, Column, Integer, String, Table
+from datetime import time
+
+from sqlalchemy import TIMESTAMP, BigInteger, Column, Integer, String, Table, func
 
 from api_server.database import db
 
@@ -8,4 +10,16 @@ users_table = Table(
     Column("destiny_membership_type", Integer, nullable=False, primary_key=True),
     Column("destiny_membership_id", BigInteger, nullable=False, primary_key=True),
     Column("display_name", String),
+    Column(
+        "created_at",
+        TIMESTAMP(timezone=True),
+        default=func.current_timestamp(),
+        nullable=False,
+    ),
+    Column(
+        "updated_at",
+        TIMESTAMP(timezone=True),
+        default=func.current_timestamp(),
+        nullable=False,
+    ),
 )
